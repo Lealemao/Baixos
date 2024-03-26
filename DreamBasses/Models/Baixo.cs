e934f96f82bb298b00ad52f39852466765b3483e
Lealemao <leonjetix126@gmail.com>
2024-03-26 18:35:12 -0300

    Arrumando uns Erros e exibindo baixos.

--- DreamBasses/Models/Baixo.cs
+++ DreamBasses/Models/Baixo.cs
@@ -5,7 +5,7 @@ public class Baixo
     public string Name { get; set; }
     public string Description { get; set; }
     public string Spec { get; set; }
-    public decimal Price { get; set; }
+    public string Price { get; set; }
     public string Img { get; set; }
-    public List<string> Marca { get; set; }
+    public string Marca { get; set; }
 }
